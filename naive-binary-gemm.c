79111843210851a0e673c2a964e833303f6b2b9a
longc-ubuntu <longch1024@gmail.com>
2017-07-13 19:06:01 +0800

    update binary gemm

--- naive-binary-gemm.c
+++ naive-binary-gemm.c
@@ -9,11 +9,6 @@
 
 #define ENCODE_BIT 32
 
-void generate_matrix(unsigned int *mat, int mat_len);
-void ones_matrix(unsigned int *mat, int mat_len);
-void binary_gemm(int m, int n, int k, unsigned int *a, int lda, unsigned int *b, int ldb, unsigned int *c, int ldc);
-void print_matrix(unsigned int *mat, int mat_len);
-void itos(unsigned int *i_var, float *s_var, int len);
 
 void generate_matrix(unsigned int *mat, int mat_len) {
     srand( (unsigned) time(0) );
@@ -24,6 +19,19 @@ void generate_matrix(unsigned int *mat, int mat_len) {
 
 void ones_matrix(unsigned int *mat, int mat_len) {
     for (int i = 0; i < mat_len; i++) {
+        mat[i] = 1;
+    }
+}
+
+void ones_matrix(int *mat, int mat_len) {
+    for (int i = 0; i < mat_len; i++) {
+        mat[i] = 1;
+    }
+}
+
+void random_matrix(int *mat, int mat_len) {
+    for (int i = 0; i < mat_len; i++) {
+    	// TODO
         mat[i] = 1;
     }
 }
@@ -45,6 +53,16 @@ void print_matrix(unsigned int *mat, int m, int n) {
     printf("\n");
 }
 
+void print_matrix(int *mat, int m, int n) {
+    for (int i = 0; i < m; i++) {
+    	for(int j = 0; j < n; j++) {
+    		printf("%d ", mat[j + i * n]);
+    	}
+        printf("\n");
+    }
+    printf("\n");
+}
+
 void print_matrix(float *mat, int m, int n) {
     for (int i = 0; i < m; i++) {
     	for(int j = 0; j < n; j++) {
@@ -55,7 +73,30 @@ void print_matrix(float *mat, int m, int n) {
     printf("\n");
 }
 
-void encode_cols(unsigned int *columns, unsigned int *columns_binary, int m, int n) {
+void encode_rows(int *columns, unsigned int *columns_binary, int m, int n) {
+    // m: row number
+    // n: col number, new col number
+    int col_bin_n = n / ENCODE_BIT; // col_bin_m: new row number
+    int i, j, k;
+
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < col_bin_n; j++) {
+            /* start index */
+            int j32 = j * ENCODE_BIT;
+            unsigned int rvalue = 0;
+            unsigned int sign;
+
+            for (k = 0; k < ENCODE_BIT; k++) {
+                sign = columns[j32 + k+ col_bin_n * i] > 0;
+                rvalue |= (sign << k);
+            }
+            /* store 32bit-encoded elem in i-th row j-th col*/
+            columns_binary[j + col_bin_n * i] = rvalue;
+        }
+    }
+}
+
+void encode_cols(int *columns, unsigned int *columns_binary, int m, int n) {
     // m: row number
     // n: col number, new col number
     int col_bin_m = m / ENCODE_BIT; // col_bin_m: new row number
@@ -68,7 +109,7 @@ void encode_cols(unsigned int *columns, unsigned int *columns_binary, int m, int
             unsigned int rvalue = 0;
             unsigned int sign;
 
-            for (k = 0; k < 1; k++) {
+            for (k = 0; k < ENCODE_BIT; k++) {
                 sign = columns[j + n * (i32 + k)] > 0;
                 rvalue |= (sign << k);
             }
@@ -84,7 +125,13 @@ void itos(unsigned int *i_var, float *s_var, int len) {
     }
 }
 
-void binary_gemm(int m, int n, int k, unsigned int *a, int lda, unsigned int *b, int ldb, unsigned int *c, int ldc) {
+void itos(int *i_var, float *s_var, int len) {
+    for (int i = 0; i < len; i++) {
+	s_var[i] = i_var[i] * 1.0;
+    }
+}
+
+void binary_gemm(int m, int n, int k, unsigned int *a, int lda, unsigned int *b, int ldb, int *c, int ldc) {
     /* column-major order */
     int i;
     unsigned int * a_ = a;
@@ -94,8 +141,11 @@ void binary_gemm(int m, int n, int k, unsigned int *a, int lda, unsigned int *b,
         for (j = 0; j < n; j++) {
             register unsigned int Cvalue = 0;
             for (l = 0; l < k; l++) {
+            	int cc = popcount(b_[l] ^ a_[l * lda + i]);
                 Cvalue += popcount(b_[l] ^ a_[l * lda + i]);
+                // printf("%d %d %d, ",b_[l], a_[l * lda + i], cc);
             }
+            // printf("%d ", Cvalue);
             b_ += ldb;
             //c[j *ldc + i] = 32 * k - 2 * Cvalue;
             c[j * ldc + i] = ENCODE_BIT * k - 2 * Cvalue;
@@ -118,16 +168,18 @@ int main(void){
     len_b = k*n;
     len_c = m*n;
 
-    unsigned int *a, *b, *c;
-    a = (unsigned int *) malloc(len_a * sizeof(unsigned int));
-    b = (unsigned int *) malloc(len_b * sizeof(unsigned int));
-    c = (unsigned int *) malloc(len_c * sizeof(unsigned int));
+    int *a, *b, *c;
+    a = (int *) malloc(len_a * sizeof(int));
+    b = (int *) malloc(len_b * sizeof(int));
+    c = (int *) malloc(len_c * sizeof(int));
 
 
     printf("A\n");
     // print_matrix(a, m, k);
     //generate_matrix(a, len_a);
     ones_matrix(a, len_a);
+    a[0] = -1;
+    a[23] = -1;
     print_matrix(a, m, k);
     printf("\n");
 
@@ -135,6 +187,9 @@ int main(void){
     // print_matrix(b, k, n);
     //generate_matrix(b, len_b);
     ones_matrix(b, len_b);
+    b[0] = -1;
+    b[35] = -1;
+    b[69] = -1;
     print_matrix(b, k, n);
     printf("\n");
 
@@ -149,9 +204,8 @@ int main(void){
     unsigned int *encoded_a, *encoded_b, *encoded_c;
     encoded_a = (unsigned int *) malloc(len_a/ENCODE_BIT * sizeof(unsigned int));
     encoded_b = (unsigned int *) malloc(len_b/ENCODE_BIT * sizeof(unsigned int));
-    encoded_c = (unsigned int *) malloc(len_c/ENCODE_BIT/ENCODE_BIT * sizeof(unsigned int));
 
-    encode_cols(a, encoded_a, m, k);
+    encode_rows(a, encoded_a, m, k);
     encode_cols(b, encoded_b, k, n);
 
     print_matrix(encoded_a, m, k/ENCODE_BIT);
@@ -159,10 +213,10 @@ int main(void){
 
     /* binary gemm */
     gettimeofday(&start, NULL);
-    binary_gemm(m/ENCODE_BIT, n/ENCODE_BIT, k/ENCODE_BIT,
-                encoded_a, len_a/ENCODE_BIT,
-                encoded_b, len_b/ENCODE_BIT,
-                encoded_c, len_c/ENCODE_BIT/ENCODE_BIT);
+    binary_gemm(m, n, k/ENCODE_BIT,
+                encoded_a, m,
+                encoded_b, k/ENCODE_BIT,
+                c, m);
     gettimeofday(&finish, NULL);
     duration = ((double)(finish.tv_sec-start.tv_sec)*1000000 + 
 		(double)(finish.tv_usec-start.tv_usec)) / 1000000;
@@ -172,7 +226,7 @@ int main(void){
 
     // binary gemm result
     printf("binary_gemm result:\n");
-    print_matrix(encoded_c, len_c/ENCODE_BIT/ENCODE_BIT);
+    print_matrix(c, m, n);
 
     /***************
      * cblas sgemm *
@@ -186,11 +240,11 @@ int main(void){
     itos(a, sa, len_a);
     itos(b, sb, len_b);
 
+    // precision benchmark
+    printf("[cblas gemm]precision benchmark for un-encoded matrix\n");
     print_matrix(sa, m, k);
     print_matrix(sb, k, n);
 
-    // precision benchmark
-    printf("[cblas gemm]precision benchmark for un-encoded matrix\n");
     cblas_sgemm(CblasColMajor,
 		CblasNoTrans,
 		CblasTrans,
@@ -223,6 +277,6 @@ int main(void){
     /* free */
     free(a); free(sa); free(encoded_a);
     free(b); free(sb); free(encoded_b);
-    free(c); free(sc); free(encoded_c);
+    free(c); free(sc);
     return 0;
 }
